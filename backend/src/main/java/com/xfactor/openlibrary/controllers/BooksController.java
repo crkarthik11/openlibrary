a6efff4f6a3a8c7e0f50163c58fc08b1cc1e6fb0
CR <crkarthik11@gmail.com>
2023-03-04 12:48:12 +0530

    jpa update

--- backend/src/main/java/com/xfactor/openlibrary/controllers/BooksController.java
+++ backend/src/main/java/com/xfactor/openlibrary/controllers/BooksController.java
@@ -2,9 +2,13 @@ package com.xfactor.openlibrary.controllers;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,13 +28,52 @@ public class BooksController {
 
     @PostMapping("/saveBook")
     public Book saveBook(@RequestBody Book book) {
-        bookRepository.save(book);
-        return book;
+        if (book.getId() == null) {
+            Book book2 = bookRepository.save(book);
+            return book2;
+        }
+        return null;
+
+    }
+
+    @PutMapping("/updateBook")
+    public Book updateBook(@RequestBody Book book) {
+        if (book.getId() != null) {
+            Book book2 = bookRepository.save(book);
+            return book2;
+        }
+        return null;
+
     }
 
     @GetMapping("/getAll")
     public List<Book> getAllBooks() {
         return bookRepository.findAll();
+    }
+
+    @GetMapping("findById/{id}")
+    public Book findById(@PathVariable Long id) {
+        Optional<Book> optionalOfBook = bookRepository.findById(id);
+        if (optionalOfBook.isPresent()) {
+            return optionalOfBook.get();
+        }
+        return null;
+    }
+
+    @GetMapping("findTotal")
+    public Long findTotal() {
+        return bookRepository.count();
+    }
+
+    @DeleteMapping("delete/{id}")
+    public void deleteBook(@PathVariable Long id) {
+        bookRepository.deleteById(id);
+    }
+
+    @GetMapping("findByisbn/{isbn}")
+    public List<Book> findByisbn(@PathVariable String isbn) {
+        List<Book> books = bookRepository.findByIsbn(isbn);
+        return books;
     }
 
 }
--- backend/src/main/java/com/xfactor/openlibrary/repositories/BookRepository.java
+++ backend/src/main/java/com/xfactor/openlibrary/repositories/BookRepository.java
@@ -1,9 +1,15 @@
 package com.xfactor.openlibrary.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.xfactor.openlibrary.domain.Book;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    List<Book> findByIsbn(String isbn);
+
+    List<Book> findByNameAndIsbn(String name, String isbn);
 
 }
