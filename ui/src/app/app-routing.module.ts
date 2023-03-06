d7ab182cf602d1cc2e19991932c1c1c30effac5b
CR <crkarthik11@gmail.com>
2023-03-06 11:57:35 +0530

    routing init

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookComponent } from './book/book.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'books',
+    component: BookComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
